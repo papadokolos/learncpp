39fc3a3010748ab8ae790aa9862cce00df215a0e
Stav Eshel <papadokolos@gmail.com>
2025-05-15 19:01:57 +0300

    Finish 8.8.q4

--- 8.8.q4.cpp
+++ 8.8.q4.cpp
@@ -17,7 +17,7 @@ namespace V2
 }
 namespace V3
 {
-    // Automatic padding with C++23 prints
+    // Automatic padding with C++23 prints, and fancy range-for
     void printNumbersDownTo1(int max_number);
 }
 
@@ -61,7 +61,8 @@ namespace V2
 {
     void printPadding(int pad_size)
     {
-        std::print("{}", std::string(static_cast<std::size_t>(pad_size), ' '));
+        std::string padding(static_cast<std::size_t>(pad_size), ' ');
+        std::print("{}", padding);
     }
 
     void printActualNumbers(int max)
@@ -89,29 +90,26 @@ namespace V2
 }
 namespace V3
 {
-    void printActualNumbers(int max)
-    {
-        int current_number{ max };
-        while (current_number >= 2)
-        {
-            std::print("{} ", current_number);
-            --current_number;
-        }
-        std::print("1"); // Printed separately to avoid extraneous space
-    }
-
     void printNumbersDownTo1(int max)
     {
         int outer{ 1 };
         while (outer <= max)
         {
             // Since clang doesn't implement `std::views::join_with` yet,
-            // I resort to ugly solution:
-            std::print(""); // Insert padding
-            for (auto num : std::views::iota(1, max + 1))
+            // I resort to ugly solution (which prints each number separately)
+
+            // Print first number with appropriate padding
+            const int pad_size{ (max - outer) * 2 + 1 };
+            const int first_number { outer };
+            std::print("{:>{}}", first_number, pad_size);
+
+            // Print remaining numbers
+            for (auto num
+                : std::views::iota(1, first_number) | std::views::reverse)
             {
-                std::print(" {}", num ); // Numbers
+                std::print(" {}", num);
             }
+
             std::println();
             ++outer;
         }

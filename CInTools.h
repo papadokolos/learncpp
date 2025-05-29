b25e85237663efb29bb40f1c1a5bbc3f3ec8785b
Stav <papadokolos@gmail.com>
2025-05-29 18:32:22 +0300

    Add getInputFromUser() to CInTools.h
    Now it can be easily used by multiple exercises.

--- CInTools.h
+++ CInTools.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#include <string_view>
+#include <print>
+#include <iostream> // for cin
+
+
 void ignoreLine();
 
 // returns true if extraction failed, false otherwise
@@ -7,3 +12,26 @@ bool clearFailedExtraction();
 
 // returns true if std::cin has unextracted input on the current line, false otherwise
 bool hasUnextractedInput();
+
+template <typename T>
+T getInputFromUser(std::string_view message)
+{
+    while (true)
+    {
+        std::print("{}", message);
+        T value {};
+        std::cin >> value;
+
+        if (clearFailedExtraction())
+        {
+            continue;
+        }
+        if (hasUnextractedInput())
+        {
+            ignoreLine();
+            continue;
+        }
+ 
+        return value;
+    }
+}

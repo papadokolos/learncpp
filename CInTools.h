53f629a64806f12716259e12b7c8bf3928ed47db
Stav Eshel <papadokolos@gmail.com>
2025-06-10 00:25:43 +0300

    Add support for options in CInTools API
    The implementation was reviewed and approved by Daniel and Amnon :D

--- CInTools.h
+++ CInTools.h
@@ -4,7 +4,6 @@
 #include <print>
 #include <iostream> // for cin
 
-
 void ignoreLine();
 
 // returns true if extraction failed, false otherwise
@@ -14,6 +13,7 @@ bool clearFailedExtraction();
 bool hasUnextractedInput();
 
 template <typename T>
+[[deprecated("Old version, use newer below")]]
 T getInputFromUser(std::string_view message)
 {
     while (true)
@@ -33,5 +33,48 @@ T getInputFromUser(std::string_view message)
         }
  
         return value;
+    }
+}
+
+
+namespace Input
+{
+    // Options vs Flags:
+    // https://unix.stackexchange.com/questions/285575/whats-the-difference-between-a-flag-an-option-and-an-argument#comment1183725_285588
+    struct GetOptions
+    {
+        bool ignoreExtraneousInput { false };
+    };
+
+    template <typename T>
+    T get(std::string_view message)
+    {
+        return get<T>(message, GetOptions{});
+    }
+
+    template <typename T>
+    T get(std::string_view message, const GetOptions& options)
+    {
+        while (true)
+        {
+            std::print("{}", message);
+            T value {};
+            std::cin >> value;
+
+            if (clearFailedExtraction())
+            {
+                continue;
+            }
+            if (hasUnextractedInput())
+            {
+                ignoreLine();
+                if (!options.ignoreExtraneousInput)
+                {
+                    continue;
+                }
+            }
+     
+            return value;
+        }
     }
 }
